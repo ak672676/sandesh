029718a9c396d890fb2f015644b8d65ceb08a904
Amit Kumar <46594792+ak672676@users.noreply.github.com>
2022-01-05 15:51:05 +0530

    User search feature added

--- lib/utils/gloal_variables.dart
+++ lib/utils/gloal_variables.dart
@@ -1,12 +1,13 @@
 import "package:flutter/material.dart";
 import 'package:sandesh/screens/add_post_screen.dart';
 import 'package:sandesh/screens/feed_screen.dart';
+import 'package:sandesh/screens/search_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
   FeedScreen(),
-  Text("Search"),
+  SearchScreen(),
   AddPostScreen(),
   Text("Notifiction"),
   Text("Profile"),
